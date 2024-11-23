ecfb82e85377b637b6bdc1b60b7b9589c8dbfd97
Willian Casa Grandi <100032236+williancasagrandi@users.noreply.github.com>
2024-11-23 17:22:44 -0300

    Update 004.sh

--- submission/004.sh
+++ submission/004.sh
@@ -1,3 +1,15 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
-bitcoin-cli getdescriptorinfo "wsh(pkh(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/100/*))"
+#chave publica extendida
+xpubKey=xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2
+
+#descritores da chave
+keyDescriptor=$(bitcoin-cli getdescriptorinfo "tr($xpubKey/0/*)")
+
+#processa a saída JSON usando jq para extrair o valor do campo descriptor
+descriptor=$(echo $keyinfosDescriptor | jq -r '.["descriptor"]')
+
+address=$(bitcoin-cli deriveaddresses $descriptor "[0,100]")
+finalkeyTaproot=$(echo $address | jq -r '.[100]')
+
+echo $finalkeyTaproot
