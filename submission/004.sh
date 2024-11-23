ad1a323c59ab92c66b626927f4ab3a80a842aca9
Willian Casa Grandi <100032236+williancasagrandi@users.noreply.github.com>
2024-11-23 14:48:46 -0300

    Update 004.sh

--- submission/004.sh
+++ submission/004.sh
@@ -1,2 +1,3 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
+bitcoin-cli getdescriptorinfo "wsh(pkh(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/100/*))"
