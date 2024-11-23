b4d3d006f3fb6c4915405368272479081900cf78
Willian Casa Grandi <100032236+williancasagrandi@users.noreply.github.com>
2024-11-23 13:20:30 -0300

    Update 002.sh

--- submission/002.sh
+++ submission/002.sh
@@ -2,5 +2,5 @@
 #   address: `1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa`
 #   message: `1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa`
 #   signature: `HCsBcgB+Wcm8kOGMH8IpNeg0H4gjCrlqwDf/GlSXphZGBYxm0QkKEPhh9DTJRp2IDNUhVr0FhP9qCqo2W0recNM=`
-bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_154 -rpcpassword=6zkgruzA7cAS verifymessage "1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa" "HCsBcgB+Wcm8kOGMH8IpNeg0H4gjCrlqwDf/GlSXphZGBYxm0QkKEPhh9DTJRp2IDNUhVr0FhP9qCqo2W0recNM=" "1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa"
+bitcoin-cli verifymessage "1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa" "HCsBcgB+Wcm8kOGMH8IpNeg0H4gjCrlqwDf/GlSXphZGBYxm0QkKEPhh9DTJRp2IDNUhVr0FhP9qCqo2W0recNM=" "1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa"
 
