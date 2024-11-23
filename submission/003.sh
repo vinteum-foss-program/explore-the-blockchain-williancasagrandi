b9b2ee2cabefc467a42fa105012f26ace15b64a1
Willian Casa Grandi <100032236+williancasagrandi@users.noreply.github.com>
2024-11-23 15:01:30 -0300

    Update 003.sh

--- submission/003.sh
+++ submission/003.sh
@@ -1,1 +1,14 @@
 # How many new outputs were created by block 123,456?
+#!/bin/bash
+
+hash=$(bitcoin-cli getblockhash 123456)
+block=$(bitcoin-cli getblock $hash)
+txids=$(echo $block | jq -r '.tx[]')
+
+totalOutputs=0
+for txid in $txids; do
+    outputs=$(bitcoin-cli getrawtransaction $txid true | jq '.vout | length')
+    totalOutputs=$((totalOutputs + outputs))
+done
+
+echo $totalOutputs
