2e1b0966ba541d36e8b61ceebf58d07b9069442a
Willian Casa Grandi <100032236+williancasagrandi@users.noreply.github.com>
2024-11-23 13:52:46 -0300

    Update setup.sh

--- submission/setup.sh
+++ submission/setup.sh
@@ -3,5 +3,5 @@ tar -xzvf bitcoin-27.1-x86_64-linux-gnu.tar.gz
 ln -s $PWD/bitcoin-27.1/bin/* /usr/local/bin/
 mkdir -p ~/.bitcoin
 echo "rpcconnect=84.247.182.145" >> ~/.bitcoin/bitcoin.conf
-echo "rpcuser=user_154" >> ~/.bitcoin/bitcoin.conf
-echo "rpcpassword=6zkgruzA7cAS" >> ~/.bitcoin/bitcoin.conf
+echo "rpcuser=classroom" >> ~/.bitcoin/bitcoin.conf
+echo "rpcpassword=R4uo1NgQGuFx" >> ~/.bitcoin/bitcoin.conf
