2cb1e5f281d53156fc8e9bed930baf61e8c83c0a
Willian Casa Grandi <100032236+williancasagrandi@users.noreply.github.com>
2024-11-25 10:12:08 -0300

    Update 005.sh

--- submission/005.sh
+++ submission/005.sh
@@ -1,2 +1,9 @@
 # Create a 1-of-4 P2SH multisig address from the public keys in the four inputs of this tx:
 #   `37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517`
+
+getTransactionHEX=$(bitcoin-cli getrawtransaction "37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517")
+decodedtransactionjSON=$(bitcoin-cli decoderawtransaction "$getTransactionHEX")
+publicKeysJSON=$(echo "$transaction" | jq -r '[.vin[].txinwitness[1]]')
+multisigJSON=$(bitcoin-cli createmultisig 1 "$publicKeys")
+
+echo "$multisigJSON" | jq -r ".address" 
