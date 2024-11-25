87ea2e71d759e49df34d3f3e2df04ea0f91445b4
Willian Casa Grandi <100032236+williancasagrandi@users.noreply.github.com>
2024-11-25 10:55:58 -0300

    Update 005.sh

--- submission/005.sh
+++ submission/005.sh
@@ -3,7 +3,7 @@
 
 getTransactionHEX=$(bitcoin-cli getrawtransaction "37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517")
 decodedtransactionjSON=$(bitcoin-cli decoderawtransaction "$getTransactionHEX")
-publicKeysJSON=$(echo "$transaction" | jq -r '[.vin[].txinwitness[1]]')
-multisigJSON=$(bitcoin-cli createmultisig 1 "$publicKeys")
+publicKeysJSON=($(echo "$decodedtransactionjSON" | jq -r '.vin[].txinwitness[1]'))
+multisigJSON=$(bitcoin-cli createmultisig 1 "[\"${publicKeysJSON[0]}\",\"${publicKeysJSON[1]}\",\"${publicKeysJSON[2]}\",\"${publicKeysJSON[3]}\"]")
 
 echo "$multisigJSON" | jq -r ".address" 
