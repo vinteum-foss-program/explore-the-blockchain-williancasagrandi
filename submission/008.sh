29b26b0d425ff81c8b710cb8e0fafdad640ed438
Willian Casa Grandi <100032236+williancasagrandi@users.noreply.github.com>
2024-11-25 12:27:23 -0300

    Update 008.sh

--- submission/008.sh
+++ submission/008.sh
@@ -1,2 +1,10 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
+txHEX=$(bitcoin-cli getrawtransaction "e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163")
+txJSON=$(bitcoin-cli decoderawtransaction $txHEX)
+publickey=$(echo $txJSON | jq -r '
+    .vin[0].txinwitness | 
+    if length > 2 then .[-1][4:70] else .[1] end
+')
+
+echo "$publickey"
